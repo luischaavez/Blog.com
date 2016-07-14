89270e55ac82b44db8e065dd1fecdc4df6383720
Luis Chávez <luischa04gmail.com>
2016-07-14 16:49:32 -0600

    Se modifico la vista para el envio de datos

--- application/views/Registro.php
+++ application/views/Registro.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+  <title><?php=$titulo?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login</title>
@@ -19,28 +20,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <form class="form-horizontal BoxReg" method="POST">
   <fieldset>
     <legend align="center"><b>Sign Up</b></legend>
+    <?php=form_open(base_url().'Reg/registro_bcrypt')?>
     <div class="form-group">
       <label for="nameu" class="col-lg-2 control-label">Name:</label>
       <div class="col-lg-10">
         <input type="text" class="form-control" name="nameu" placeholder="Juan">
+        <p><?php=form_error('nameu')?></p>
       </div>
     </div>
     <div class="form-group">
       <label for="email" class="col-lg-2 control-label">Email:</label>
       <div class="col-lg-10">
         <input type="text" class="form-control" name="email" placeholder="example@example.com">
+        <p><?php=form_error('email')?></p>
       </div>
     </div>
      <div class="form-group">
       <label for="user" class="col-lg-2 control-label">User name:</label>
       <div class="col-lg-10">
         <input type="text" class="form-control" name="user" placeholder="Juan_197">
+        <p><?php=form_error('user')?></p>
       </div>
     </div>
     <div class="form-group">
       <label for="pass" class="col-lg-2 control-label">Password:</label>
       <div class="col-lg-10">
         <input type="password" class="form-control" name="pass" placeholder="********">
+        <p><?php=form_error('pass')?></p>
+        <input type="hidden" name="token" value="<?php=$token?>" />
       </div>
     </div>
      <div class="checkbox col-lg-10">
@@ -51,11 +58,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="form-group">
       <div class="col-lg-10 col-lg-offset-2">
         <button type="reset" class="btn btn-default">Cancel</button>
-        <button type="submit" class="btn btn-success">Submit</button>
+        <button type="submit" class="btn btn-success" value="Guardar">Submit</button>
       </div>
     </div>
   </fieldset>
 </form>
+<?php=form_close()?>
+<?anchor(base_url/().'Reg/registro_bcrypt', 'Registro')?>
 
 
 
