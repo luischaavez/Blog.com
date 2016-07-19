4c5b032b3c31caef059b988b0459c805e6e81d6f
Luis Chávez <luischa04gmail.com>
2016-07-19 14:55:04 -0600

    Se realizo el Log Out del usuario

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -71,4 +71,11 @@ class Login extends CI_Controller {
   		}
   	}
   }
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect(base_url().'Login/index');
+	}
+
 }
--- application/views/estaticos/Navbar.php
+++ application/views/estaticos/Navbar.php
@@ -63,15 +63,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </li>
                         <li class="divider"></li>
                         <li>
+
                             <div class="navbar-login navbar-login-session">
                                 <div class="row">
                                     <div class="col-lg-12">
+
                                         <p>
-                                            <a href="#" class="btn btn-danger btn-block">Log Out</a>
+                                            <a href="<?php echo base_url()?>Login/logout" class="btn btn-danger btn-block">Log Out</a>
                                         </p>
+
                                     </div>
                                 </div>
                             </div>
+                            <?php echo form_close() ?>
                         </li>
                     </ul>
                 </li>
