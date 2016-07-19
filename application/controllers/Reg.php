6582dcd45275c66e12ec5afd974b82161e09cb99
Luis Chávez <luischa04gmail.com>
2016-07-19 13:00:27 -0600

    Se elimaron las rutas a las librerias y helpers

--- application/controllers/Reg.php
+++ application/controllers/Reg.php
@@ -8,9 +8,6 @@ class Reg extends CI_Controller {
 		parent::__construct();
 		/*Cargar la libreria Bcrypt*/
 		$this->load->library('bcrypt');
-		$this->load->model('Register_model');
-		$this->load->library(array('form_validation', 'session'));
-		$this->load->helper(array('url', 'form'));
 		$this->load->database('default');
 	}
 
@@ -45,7 +42,7 @@ class Reg extends CI_Controller {
 			$this->form_validation->set_rules('user', 'user', 'required|trim|max_length[50]');
 			$this->form_validation->set_rules('pass', 'password', 'required|trim|min_length[6]|max_length[50]');
 
-			
+
 			$this->form_validation->set_message('required', 'This field must not be empty');
 			$this->form_validation->set_message('min_length', 'The field needs to have at less 6 chars');
 			$this->form_validation->set_message('max_length', 'The field can not have more than 50 chars');
@@ -64,7 +61,7 @@ class Reg extends CI_Controller {
 		//Probar si la contraseña se encripto
 		if ($this->bcrypt->check_password($password, $hash))
 		{
- 
+
 			$insert_pass = $this->Register_model->save_data($name, $email, $username, $hash);
 			if ($insert_pass)
 			{
@@ -76,7 +73,7 @@ class Reg extends CI_Controller {
 			throw new Exception("Error Processing request", 1);
 			echo Exception;
 		}
- 
+
 		}
 
 		}
