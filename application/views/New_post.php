b7dc74cd6fe1aa12668204dcbd7bfbb2a524c159
Luis Chávez <luischa04gmail.com>
2016-07-24 20:46:05 -0600

    Se añadieros algunos scripts

--- application/views/New_post.php
+++ application/views/New_post.php
@@ -39,3 +39,9 @@
 </div>
 <?php echo form_close() ?>
 </fieldset>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+</body>
+</html>
